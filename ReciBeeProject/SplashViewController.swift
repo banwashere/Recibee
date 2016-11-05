b0e9d119d325d94c2eceea0891c0ee50cdaa68dc
banwashere <banwashere@gmail.com>
2016-11-06 00:53:11 +0800

    ReciBeeProject/

---
+++ ReciBeeProject/SplashViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReciBeeProject
+//
+//  Created by Chee Kok Ban on 29/10/16.
+//  Copyright © 2016 Chee Kok Ban. All rights reserved.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
